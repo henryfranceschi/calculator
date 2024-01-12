bdb3b3e93ed32963624d51c1d7e3854d6d9e415e
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2024-01-12 23:41:06 +0100

    added ast

---
+++ src/ast.rs
@@ -0,0 +1,54 @@
+use crate::lexer::span::Span;
+
+pub struct Spanned<T> {
+    pub node: T,
+    pub span: Span,
+}
+
+impl<T> Spanned<T> {
+    pub fn new(node: T, span: Span) -> Self {
+        Self { node, span }
+    }
+}
+
+pub struct UnOp(Spanned<UnOpKind>);
+
+impl UnOp {
+    pub fn new(span: Span, kind: UnOpKind) -> Self {
+        Self(Spanned::new(kind, span))
+    }
+}
+pub enum UnOpKind {
+    Neg,
+}
+
+pub struct BinOp(Spanned<BinOpKind>);
+
+impl BinOp {
+    pub fn new(span: Span, kind: BinOpKind) -> Self {
+        Self(Spanned::new(kind, span))
+    }
+}
+
+pub enum BinOpKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+}
+
+pub struct Expr(Spanned<ExprKind>);
+
+impl Expr {
+    pub fn new(span: Span, kind: ExprKind) -> Self {
+        Self(Spanned::new(kind, span))
+    }
+}
+
+pub enum ExprKind {
+    Number(f64),
+    Unary(UnOp, Box<Expr>),
+    Binary(BinOp, Box<Expr>, Box<Expr>),
+}
+
