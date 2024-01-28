5fa56e6c5725e63869a52b64ebd293aefe41e1d0
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2024-01-28 23:39:14 +0100

    added error reporting function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use calculator::lexer::span::Span;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn report_error(message: &str, span: Span, source: &str) {
+    let line = span.starting_line_number(source);
+    let column = span.starting_column_number(source);
+
+    // Lines will not yield empty lines, so we have to handle it like this for the case where the
+    // source ends with a newline.
+    if let Some(src_line) = source.lines().nth(line - 1) {
+        eprintln!("[{}:{}] {}", line, column, src_line);
+    }
+
+    eprintln!("{}", message);
 }
