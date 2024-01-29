405325d4e8c69c2f1040d46211cf506fd0603c73
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2024-01-29 23:34:55 +0100

    added cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
-use calculator::lexer::span::Span;
+use calculator::{
+    codegen,
+    lexer::{span::Span, Lexer},
+    parser::Parser,
+    vm::Vm,
+};
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("usage: {} <expression>", env!("CARGO_BIN_NAME"));
+        return;
+    }
+
+    let source = args[1].as_str();
+    let mut parser = Parser::new(Lexer::new(source));
+    match parser.parse() {
+        Ok(expr) => {
+            let mut vm = Vm::new(codegen::generate(&expr));
+            match vm.run() {
+                Ok(value) => println!("{}", value),
+                Err(err) => eprintln!("runtime error: {:?}", err),
+            }
+        }
+        Err(err) => {
+            report_error(err.message(), err.span(), source);
+        }
+    }
 }
 
 fn report_error(message: &str, span: Span, source: &str) {
