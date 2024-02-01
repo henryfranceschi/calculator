f20f9f152f1f74e185443befd1aaae0e9dcb7519
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2024-02-01 23:58:04 +0100

    updated error reporting function

--- src/main.rs
+++ src/main.rs
@@ -33,18 +33,22 @@ fn report_error(message: &str, span: Span, source: &str) {
     let column = span.starting_column_number(source);
     let margin_width = 3 + digit_count(line as u32) + digit_count(column as u32);
 
-    // Lines will not yield empty lines, so we have to handle it like this for the case where the
-    // source ends with a newline.
-    if let Some(src_line) = source.lines().nth(line - 1) {
-        eprintln!("[{}:{}] {}", line, column, src_line);
-        eprintln!(
-            "{} {} {message:?}",
-            str::repeat(" ", margin_width as usize + column),
-            str::repeat("^", span.len()),
-        );
-    } else {
-        eprintln!("[eof]");
-        eprintln!("{message}");
+    eprintln!("error: {message}");
+
+    match source.lines().nth(line - 1) {
+        Some(src_line) if !span.is_empty() => {
+            eprintln!("[{}:{}] {}", line, column, src_line);
+            eprintln!(
+                "{} {}",
+                str::repeat(" ", margin_width as usize + column),
+                str::repeat("^", span.len()),
+            );
+        }
+        // The only kind of token that has an empty span is eof which is always the last token
+        // yielded by the lexer.
+        _ => {
+            eprintln!("[eof]");
+        }
     }
 }
 
