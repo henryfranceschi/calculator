5bf12caca547625e2e921ddb36d1023b293f7320
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2024-03-07 23:56:24 +0100

    added display implementation for token kind

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -20,3 +20,29 @@ pub enum TokenKind {
     Percent,
     Eof,
 }
+
+impl TokenKind {
+    /// If the `lexeme`s of all `Token`s with this kind are identical.
+    pub fn is_uniform(self) -> bool {
+        !matches!(self, Self::Number)
+    }
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            TokenKind::LParen => "(",
+            TokenKind::RParen => ")",
+            TokenKind::Semicolon => ";",
+            TokenKind::Number => "<number>",
+            TokenKind::Plus => "+",
+            TokenKind::Minus => "-",
+            TokenKind::Star => "*",
+            TokenKind::Slash => "/",
+            TokenKind::Percent => "%",
+            TokenKind::Eof => "<eof>",
+        };
+
+        f.write_str(s)
+    }
+}
